50f37560cd923f7c7805e80ba43c4e2c33f92e1f
Noah765 <noland62007@gmail.com>
2025-01-06 22:33:07 +0100

    fixed the interact pages round display

--- lib/game/interact/page.dart
+++ lib/game/interact/page.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:gap/gap.dart';
 import 'package:geocibus/game/interact/character.dart';
 import 'package:geocibus/game/interact/chat.dart';
 import 'package:geocibus/game/interact/resources.dart';
+import 'package:geocibus/game/interact/top.dart';
 import 'package:geocibus/models/game.dart';
 import 'package:geocibus/models/region.dart';
-import 'package:geocibus/widgets/settings_button.dart';
 import 'package:provider/provider.dart';
 
 class InteractPage extends StatelessWidget {
@@ -19,29 +18,13 @@ class InteractPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [ChangeNotifierProvider.value(value: game), Provider.value(value: region)],
-      child: Scaffold(
+      child: const Scaffold(
         body: Padding(
-          padding: const EdgeInsets.all(8),
+          padding: EdgeInsets.all(8),
           child: Column(
             children: [
-              Row(
-                children: [
-                  IconButton(
-                    icon: const FaIcon(FontAwesomeIcons.arrowLeft),
-                    onPressed: Navigator.of(context).pop,
-                    tooltip: 'Zurück zur Karte',
-                  ),
-                  Card.filled(
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 12),
-                      child: Text('${game.month} des Jahres ${game.round}/10'),
-                    ),
-                  ),
-                  const Spacer(),
-                  const SettingsButton(),
-                ],
-              ),
-              const Expanded(
+              InteractTop(),
+              Expanded(
                 child: Row(
                   children: [
                     InteractCharacter(),
@@ -53,12 +36,6 @@ class InteractPage extends StatelessWidget {
                           Expanded(child: Chat()),
                         ],
                       ),
-                      //child: Column(
-                      //  children: [
-                      //    const Row(crossAxisAlignment: CrossAxisAlignment.start, children: [Expanded(child: InteractMessage()), Gap(24), InteractResources()]),
-                      //    if (!region.isTradeBlocked) InteractDistribute(region: region) else const Text('Handel blockiert'),
-                      //  ],
-                      //),
                     ),
                   ],
                 ),
---
+++ lib/game/interact/top.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:geocibus/models/game.dart';
+import 'package:geocibus/widgets/settings_button.dart';
+import 'package:provider/provider.dart';
+
+class InteractTop extends StatelessWidget {
+  const InteractTop({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final game = context.watch<Game>();
+
+    return Row(
+      children: [
+        IconButton(
+          icon: const FaIcon(FontAwesomeIcons.arrowLeft),
+          onPressed: Navigator.of(context).pop,
+          tooltip: 'Zurück zur Karte',
+        ),
+        Card.filled(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 12),
+            child: Text('${game.month} des Jahres ${game.round}/10'),
+          ),
+        ),
+        const Spacer(),
+        const SettingsButton(),
+      ],
+    );
+  }
+}
