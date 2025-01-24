794eaab73be1ccd9dc7b39273cd36d9e9ab883d3
Noah765 <noland62007@gmail.com>
2025-01-24 17:04:54 +0100

    fixed slider overflow bug

--- lib/game/main/exchange.dart
+++ lib/game/main/exchange.dart
@@ -40,10 +40,18 @@ class _PopupState extends State<_Popup> {
   var _water = 0.0;
   var _food = 0.0;
 
-  int get _waterMax => (widget.game.money - _food.round() * widget.game.foodPrice) ~/ widget.game.waterPrice;
-  int get _foodMax => (widget.game.money - _water.round() * widget.game.waterPrice) ~/ widget.game.foodPrice;
+  int get _waterMaxPossible => (widget.game.money - _food.round() * widget.game.foodPrice) ~/ widget.game.waterPrice;
+  int get _foodMaxPossible => (widget.game.money - _water.round() * widget.game.waterPrice) ~/ widget.game.foodPrice;
+  double get _waterMax => max(_waterMaxPossible.toDouble(), widget.game.additionalWaterMaximum.toDouble());
+  double get _foodMax => max(_foodMaxPossible.toDouble(), widget.game.additionalFoodMaximum.toDouble());
   int get _waterForFoodMax => (-(widget.game.additionalFoodMaximum * widget.game.foodPrice - widget.game.money) / widget.game.waterPrice).ceil();
   int get _foodForWaterMax => (-(widget.game.additionalWaterMaximum * widget.game.waterPrice - widget.game.money) / widget.game.foodPrice).ceil();
+
+  void _updateValue(VoidCallback callback) {
+    setState(() => callback());
+    _water = min(_water, _waterMax);
+    _food = min(_food, _foodMax);
+  }
 
   bool get _isExchangePossible => (_water.round() * widget.game.waterPrice).ceil() + (_food.round() * widget.game.foodPrice).ceil() <= widget.game.money;
 
@@ -86,11 +94,11 @@ class _PopupState extends State<_Popup> {
                     height: sliderHeight,
                     child: BidirectionalSlider(
                       value: _water,
-                      secondaryTrackValueRight: _waterMax.toDouble(),
-                      onChanged: (value) => setState(() => _water = value),
-                      snapValues: [-widget.game.water.toDouble(), _waterForFoodMax.toDouble(), 0, _waterMax.toDouble(), widget.game.additionalWaterMaximum.toDouble()],
+                      secondaryTrackValueRight: _waterMaxPossible.toDouble(),
+                      onChanged: (value) => _updateValue(() => _water = value),
+                      snapValues: [-widget.game.water.toDouble(), _waterForFoodMax.toDouble(), 0, _waterMaxPossible.toDouble(), widget.game.additionalWaterMaximum.toDouble()],
                       leftMax: widget.game.water.toDouble(),
-                      rightMax: max(_waterMax.toDouble(), widget.game.additionalWaterMaximum.toDouble()),
+                      rightMax: _waterMax,
                     ),
                   ),
                   const Gap(8),
@@ -98,11 +106,11 @@ class _PopupState extends State<_Popup> {
                     height: sliderHeight,
                     child: BidirectionalSlider(
                       value: _food,
-                      secondaryTrackValueRight: _foodMax.toDouble(),
-                      onChanged: (value) => setState(() => _food = value),
-                      snapValues: [-widget.game.food.toDouble(), _foodForWaterMax.toDouble(), 0, _foodMax.toDouble(), widget.game.additionalFoodMaximum.toDouble()],
+                      secondaryTrackValueRight: _foodMaxPossible.toDouble(),
+                      onChanged: (value) => _updateValue(() => _food = value),
+                      snapValues: [-widget.game.food.toDouble(), _foodForWaterMax.toDouble(), 0, _foodMaxPossible.toDouble(), widget.game.additionalFoodMaximum.toDouble()],
                       leftMax: widget.game.food.toDouble(),
-                      rightMax: max(_foodMax.toDouble(), widget.game.additionalFoodMaximum.toDouble()),
+                      rightMax: _foodMax,
                     ),
                   ),
                 ],
